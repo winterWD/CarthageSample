53071c99f19d34649538525bc96c204b22921dd5
winter <winter.wei@hey900.com>
2017-07-24 14:17:12 +0800

    add test

--- CarthageSampleTests/CarthageSampleTests.swift
+++ CarthageSampleTests/CarthageSampleTests.swift
@@ -6,31 +6,24 @@
 //  Copyright © 2017年 DJ. All rights reserved.
 //
 
-import XCTest
-@testable import CarthageSample
+import Quick
+import Nimble
+import CarthageSample
 
-class CarthageSampleTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+class CarthageSampleTests: QuickSpec {
+    override func spec() {
+        describe("RGBUIColor") {
+            it("is a correct representation of the values") {
+                let thoughtbotRed = UIColor(
+                    red: CGFloat(195/255),
+                    green: CGFloat(47/255),
+                    blue: CGFloat(52/255),
+                    alpha: 1
+                )
+                let color = RGBUIColor(red: 195, green: 47, blue: 52)
+                
+                expect(color).to(equal(thoughtbotRed))
+            }
         }
     }
-    
 }
